03a2d431748ace1604166fd5cfc41c82bb9db24b
geekchen <cscbeyond@sina.com>
2019-12-09 22:00:57 +0800

    精简项目

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
 
+/*
+函数式定义组件
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     hello world
     </div>
   );
+}
+*/
+// 这样也可以
+// class App extends React.Component { // 这样也可以
+class App extends Component {
+  render() {
+    return <div className="App">hello, geek chen</div>;
+  }
 }
 
 export default App;
